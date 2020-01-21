f1c68ef4de4997bcc1a828db556cfbf7f4ad27cf
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-01-21 21:14:28 +0100

    Fix: weird projection
    There was a problem with the projection. This was fixed along with some
    refactoring of the get_pixel function.

--- src/renderer/get_pixel.c
+++ src/renderer/get_pixel.c
@@ -6,7 +6,7 @@
 /*   By: aaugusti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/17 15:36:58 by aaugusti          #+#    #+#             */
-/*   Updated: 2020/01/21 18:21:38 by abe              ###   ########.fr       */
+/*   Updated: 2020/01/21 21:13:19 by abe              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,18 @@
 
 t_color			get_pixel(t_vec2i pixel, t_info *info)
 {
-	t_ray	ray;
-	double	ar;
-	double	fov_factor;
+	t_ray		ray;
+	t_vec3f		cam_loc;;
+	double		fov_factor;
 
 	fov_factor = tan(info->current_cam->fov / 2 * (M_PI / 180));
-	ray.origin.x = info->current_cam->location.x;
-	ray.origin.y = info->current_cam->location.y;
+	cam_loc = info->current_cam->location;
+	ray.origin.x = cam_loc.x + (2 * ((pixel.x + 0.5) / info->mapinfo.res.x) - 1)  * fov_factor;
+	ray.origin.y = cam_loc.y + (1 - (2 *((pixel.y + 0.5) / info->mapinfo.res.y))) * fov_factor;
 	if (info->mapinfo.res.x > info->mapinfo.res.y)
-	{
-		ar = ((double)info->mapinfo.res.x) / ((double)info->mapinfo.res.y);
-		ray.origin.x += (2 * ((pixel.x + 0.5) / info->mapinfo.res.x - 1)) * ar * fov_factor;
-		ray.origin.y += 1 - (2 *((pixel.y + 0.5) / info->mapinfo.res.y)) * fov_factor;
-	}
+		ray.origin.x *= info->mapinfo.res.x / (double)info->mapinfo.res.y;
 	else
-	{
-		ar = ((double)info->mapinfo.res.y) / ((double)info->mapinfo.res.x);
-		ray.origin.x += (2 * ((pixel.x + 0.5) / info->mapinfo.res.x - 1)) * fov_factor;
-		ray.origin.y += 1 - (2 *((pixel.y + 0.5) / info->mapinfo.res.y)) * ar *fov_factor;
-	}
+		ray.origin.y *= info->mapinfo.res.y / (double)info->mapinfo.res.x;
 	ray.origin.z = info->current_cam->location.z - 1;
 	ray_calc_dir(&ray, info->current_cam->location);
 	return (ray_cast(info, ray));
