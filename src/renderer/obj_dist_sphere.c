c56a37a3d0bc8f892d67d3d194c7b04f15610525
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-01-21 16:59:56 +0100

    Misc

--- src/renderer/obj_dist_sphere.c
+++ src/renderer/obj_dist_sphere.c
@@ -6,7 +6,7 @@
 /*   By: aaugusti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 16:42:25 by aaugusti          #+#    #+#             */
-/*   Updated: 2020/01/21 14:59:02 by abe              ###   ########.fr       */
+/*   Updated: 2020/01/21 16:51:52 by aaugusti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,6 @@ static bool	does_intersect(t_object *sp, t_ray ray)
 
 double		obj_dist_sphere(t_object *sp, t_ray ray)
 {
-	double	dist_to_center;
-
-	(void)dist_to_center;
 	if (!does_intersect(sp, ray))
 		return (INFINITY);
 	return (1);
