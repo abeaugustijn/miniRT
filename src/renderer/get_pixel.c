32bd33beaa3fd5934ff321bf65a77caae5539526
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-01-21 15:45:47 +0100

    Fix: trying to fix the skewering

--- src/renderer/get_pixel.c
+++ src/renderer/get_pixel.c
@@ -6,7 +6,7 @@
 /*   By: aaugusti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/17 15:36:58 by aaugusti          #+#    #+#             */
-/*   Updated: 2020/01/21 12:29:27 by aaugusti         ###   ########.fr       */
+/*   Updated: 2020/01/21 15:04:25 by abe              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,19 @@ t_color			get_pixel(t_vec2i pixel, t_info *info)
 	double	ar;
 	double	fov_factor;
 
+	fov_factor = tan(info->current_cam->fov / 2 * (M_PI / 180));
 	if (info->mapinfo.res.x > info->mapinfo.res.y)
-		ar = info->mapinfo.res.x / info->mapinfo.res.y;
+	{
+		ar = ((double)info->mapinfo.res.x) / ((double)info->mapinfo.res.y);
+		ray.origin.x = (2 * ((pixel.x + 0.5) / info->mapinfo.res.x - 1)) * ar * fov_factor;
+		ray.origin.y = 1 - (2 *((pixel.y + 0.5) / info->mapinfo.res.y)) * fov_factor;
+	}
 	else
-		ar = info->mapinfo.res.y / info->mapinfo.res.x;
-	fov_factor = tan(info->current_cam->fov / 2 * M_PI / 180);
-	ray.origin.x = (2 * ((pixel.x + 0.5) / info->mapinfo.res.x - 1)) * ar * fov_factor;
-	ray.origin.y = (1 - 2 *((pixel.y + 0.5) / info->mapinfo.res.y)) * fov_factor;
+	{
+		ar = ((double)info->mapinfo.res.y) / ((double)info->mapinfo.res.x);
+		ray.origin.x = (2 * ((pixel.x + 0.5) / info->mapinfo.res.x - 1)) * fov_factor;
+		ray.origin.y = 1 - (2 *((pixel.y + 0.5) / info->mapinfo.res.y)) * ar *fov_factor;
+	}
 	ray.origin.z = info->current_cam->location.z - 1;
 	ray_calc_dir(&ray, info->current_cam->location);
 	return (ray_cast(info, ray));
--- src/renderer/obj_dist_sphere.c
+++ src/renderer/obj_dist_sphere.c
@@ -6,7 +6,7 @@
 /*   By: aaugusti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 16:42:25 by aaugusti          #+#    #+#             */
-/*   Updated: 2020/01/21 11:56:02 by aaugusti         ###   ########.fr       */
+/*   Updated: 2020/01/21 14:59:02 by abe              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static bool	does_intersect(t_object *sp, t_ray ray)
 	double	ray_dist;
 
 	ray_dist = dist_line_point(ray, sp->location);
-	return (ray_dist < sp->size);
+	return (ray_dist < sp->size / 2);
 }
 
 double		obj_dist_sphere(t_object *sp, t_ray ray)
