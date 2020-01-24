d2335c389aa0789b0e6854dea5c4ef66a5bcad12
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-01-24 16:16:07 +0100

    Revert "Fix?: malloc info struct"
    This reverts commit 39d9387c5883a24108d90e15725685cd5d8dd6e3.

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
+/*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
-/*                                                     +:+                    */
-/*   By: abe <marvin@42.fr>                           +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2020/01/14 19:23:08 by abe            #+#    #+#                */
-/*   Updated: 2020/01/24 13:52:39 by aaugusti         ###   ########.fr       */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abe <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/14 19:23:08 by abe               #+#    #+#             */
+/*   Updated: 2020/01/20 14:30:51 by aaugusti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,24 @@
 #include <libft.h>
 #include <liblist.h>
 #include <mlx.h>
-#include <stdlib.h>
 
 int	main(int argc, char *argv[])
 {
-	t_info		*info;
+	t_info		info;
 
 	if (argc != 2 && argc != 3)
 		print_error("Invalid amount of arguments\n");
-	info = malloc(sizeof(t_info));
-	ft_bzero(info, sizeof(t_info));
+	ft_bzero(&info, sizeof(t_info));
 	if (argc == 3)
 	{
 		if (ft_strcmp(argv[2], "--save"))
 			print_error("Invalid argument given\n");
 		else
-			info->mapinfo.do_save = true;
+			info.mapinfo.do_save = true;
 	}
-	parse_input(argv[1], info);
-	if (init_mlx(&info->mlx_info, info))
+	parse_input(argv[1], &info);
+	if (init_mlx(&info.mlx_info, &info))
 		print_error("Error opening window\n");
-	info->current_cam = info->cameras->content;
-	mlx_loop(info->mlx_info.mlx);
-	free(info);
+	info.current_cam = info.cameras->content;
+	mlx_loop(info.mlx_info.mlx);
 }
