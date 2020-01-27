e1c9c25e2b17c49ff3140ba16cfe0d28113214dc
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-01-27 15:05:57 +0100

    Feat: did ambient

--- src/renderer/ray_cast.c
+++ src/renderer/ray_cast.c
@@ -6,7 +6,7 @@
 /*   By: aaugusti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 16:30:00 by aaugusti          #+#    #+#             */
-/*   Updated: 2020/01/27 14:14:28 by aaugusti         ###   ########.fr       */
+/*   Updated: 2020/01/27 15:05:21 by aaugusti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <math.h>
 #include <libft.h>
 
-t_color	ray_cast(t_info *info, t_ray ray)
+static	t_rayres	ray_cast_object(t_info *info, t_ray ray)
 {
 	t_list		*current;
 	t_object	*closest;
@@ -37,6 +37,19 @@ t_color	ray_cast(t_info *info, t_ray ray)
 		current = current->next;
 	}
 	if (!closest)
+		return (rayres_inf());
+	return (rayres_closest);
+}
+
+t_color	ray_cast(t_info *info, t_ray ray)
+{
+	t_rayres	obj;
+	t_color		res;
+
+	obj = ray_cast_object(info, ray);
+	if (obj.dist == INFINITY)
 		return (col_new(0, 0, 0));
-	return (rayres_closest.color);
+	res = col_mix_ambient(obj.color, info->mapinfo.ambient_color);
+	res = col_multiply(res, info->mapinfo.ambient_ratio);
+	return (res);
 }
