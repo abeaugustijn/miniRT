0391d073a48655f41a1a62e8e512c238b4b4403e
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-01-28 12:37:54 +0100

    Fix?: tyring to do the weird projection

--- src/renderer/obj_dist_sphere.c
+++ src/renderer/obj_dist_sphere.c
@@ -6,7 +6,7 @@
 /*   By: aaugusti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 16:42:25 by aaugusti          #+#    #+#             */
-/*   Updated: 2020/01/27 18:59:36 by abe              ###   ########.fr       */
+/*   Updated: 2020/01/28 12:25:00 by aaugusti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 t_rayres		obj_dist_sphere(t_object *sp, t_ray ray)
 {
 	t_vec3f		closest;
-	t_vec3f		p;
+	double		dist_hit;
 	double		radius;
 	double		y;
 	double		center_dist;
@@ -28,8 +28,12 @@ t_rayres		obj_dist_sphere(t_object *sp, t_ray ray)
 	if (center_dist > radius)
 		return (rayres_inf());
 	y = sqrt(pow(radius, 2) - pow(center_dist, 2));
-	p = vec_add(ray.origin, sp->location);
-	p = vec_sub(p, vec_multiply(ray.direction, y));
-	return (rayres_new_dist(p, sp->color, vec_dist(ray.origin, p)));
+	dist_hit = vec_dist(ray.origin, sp->location) - y;
+	/*p = vec_add(ray.origin, sp->location);*/
+	/*p = vec_add(p, vec_multiply(ray.direction, y));*/
+	/*return (rayres_new_dist(p, sp->color, vec_dist(ray.origin, p)));*/
+	return (rayres_new_dist(
+				vec_add(ray.origin, vec_multiply(ray.direction, dist_hit)),
+				sp->color, dist_hit));
 }
 
