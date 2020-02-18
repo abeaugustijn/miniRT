63d59c85c14083c59e70aaaba10175e4f8ab061f
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-02-18 13:00:50 +0100

    Fix: spheres when looked at close up
    Used the camera location in stead of the ray origin for the sphere dist
    calculations. This fixed the issue where spheres would look weird if the
    camera was close.

--- src/obj_dist/obj_dist_sphere.c
+++ src/obj_dist/obj_dist_sphere.c
@@ -6,12 +6,14 @@
 /*   By: aaugusti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 16:42:25 by aaugusti          #+#    #+#             */
-/*   Updated: 2020/02/17 16:33:19 by aaugusti         ###   ########.fr       */
+/*   Updated: 2020/02/18 12:54:27 by aaugusti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <miniRT.h>
 #include <math.h>
+
+#define CAM info->current_cam
 
 t_rayres		obj_dist_sphere(t_object *sp, t_ray ray, t_info *info)
 {
@@ -21,10 +23,10 @@ t_rayres		obj_dist_sphere(t_object *sp, t_ray ray, t_info *info)
 	t_vec3f	p;
 
 	(void)info;
-	t = vec_dotp(vec_sub(sp->location, ray.origin), ray.direction);
+	t = vec_dotp(vec_sub(sp->location, CAM->location), ray.direction);
 	if (t < 0)
 		return (rayres_inf());
-	p = vec_add(ray.origin, vec_multiply(ray.direction, t));
+	p = vec_add(CAM->location, vec_multiply(ray.direction, t));
 	y = vec_len(vec_sub(sp->location, p));
 	if (y > sp->size / 2)
 		return (rayres_inf());
