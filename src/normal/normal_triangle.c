fc313031f33c8f92602c0c8c1e1269219b04a21e
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-02-24 17:13:57 +0100

    Feat: added some optimization for the triangle normal

--- src/normal/normal_triangle.c
+++ src/normal/normal_triangle.c
@@ -6,14 +6,16 @@
 /*   By: abe <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/28 21:39:49 by abe               #+#    #+#             */
-/*   Updated: 2020/02/17 13:58:23 by aaugusti         ###   ########.fr       */
+/*   Updated: 2020/02/24 17:10:40 by abe              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <miniRT.h>
 
 /*
-**	Gets the normal vector of a triangle.
+**	Gets the normal vector of a triangle. If the normal stored in the triangle
+**	is not normalized, we assume that it was not generated yet and generate
+**	it again.
 **
 **	@param {t_rayres} rayres
 **
@@ -27,8 +29,12 @@ t_vec3f	normal_triangle(t_rayres rayres, t_info *info)
 	t_vec3f cb;
 
 	(void)info;
-	ca = vec_from_to(rayres.obj->points[2], rayres.obj->points[0]);
-	cb = vec_from_to(rayres.obj->points[2], rayres.obj->points[1]);
-	res = vec_crossp(ca, cb);
-	return (vec_normalize(res));
+	if (!check_normalized(rayres.obj->orientation))
+	{
+		ca = vec_from_to(rayres.obj->points[2], rayres.obj->points[0]);
+		cb = vec_from_to(rayres.obj->points[2], rayres.obj->points[1]);
+		res = vec_crossp(ca, cb);
+		rayres.obj->orientation = vec_normalize(res);
+	}
+	return (rayres.obj->orientation);
 }
