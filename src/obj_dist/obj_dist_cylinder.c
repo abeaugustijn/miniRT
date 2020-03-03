d56080c3996062b9ee8a67bf05f17390715ae227
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-03-03 10:30:31 +0100

    Fix: #2 wip

--- src/obj_dist/obj_dist_cylinder.c
+++ src/obj_dist/obj_dist_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: aaugusti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 16:42:25 by aaugusti          #+#    #+#             */
-/*   Updated: 2020/03/02 10:33:46 by aaugusti         ###   ########.fr       */
+/*   Updated: 2020/03/03 09:13:48 by abe              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,16 @@
 **	@return {double}
 */
 
-static double	get_closest_ray(t_object *cy, t_ray ray, t_vec3f ray_to_cy)
-{
-	double	t;
+/*static double	get_closest_ray(t_object *cy, t_ray ray, t_vec3f ray_to_cy)*/
+/*{*/
+	/*double	t;*/
 
-	t = (vec_dotp(ray.direction, ray_to_cy) * vec_dotp(cy->orientation, cy->orientation)
-			- vec_dotp(ray.direction, cy->orientation) * vec_dotp(cy->orientation, ray_to_cy)) / 
-		(vec_dotp(ray.direction, ray.direction) * vec_dotp(cy->orientation, cy->orientation)
-		 	- pow(vec_dotp(ray.direction, cy->orientation), 2));
-	return (t);
-}
+	/*t = (vec_dotp(ray.direction, ray_to_cy) * vec_dotp(cy->orientation, cy->orientation)*/
+			/*- vec_dotp(ray.direction, cy->orientation) * vec_dotp(cy->orientation, ray_to_cy)) / */
+		/*(vec_dotp(ray.direction, ray.direction) * vec_dotp(cy->orientation, cy->orientation)*/
+			 /*- pow(vec_dotp(ray.direction, cy->orientation), 2));*/
+	/*return (t);*/
+/*}*/
 
 
 /*
@@ -49,38 +49,44 @@ static double	get_closest_ray(t_object *cy, t_ray ray, t_vec3f ray_to_cy)
 **	@return {t_vec3f} //TODO: maybe return t?
 */
 
-static t_vec3f	get_closest_cy(t_object *cy, t_ray ray, t_vec3f ray_to_cy)
-{
-	double	t;
+/*static t_vec3f	get_closest_cy(t_object *cy, t_ray ray, t_vec3f ray_to_cy)*/
+/*{*/
+	/*double	t;*/
 
-	t = (vec_dotp(ray.direction, cy->orientation) * vec_dotp(ray.direction, ray_to_cy)
-			- vec_dotp(cy->orientation, ray_to_cy) * vec_dotp(ray.direction, ray.direction)) / 
-		(vec_dotp(ray.direction, ray.direction) * vec_dotp(cy->orientation, cy->orientation)
-		 	- pow(vec_dotp(ray.direction, cy->orientation), 2));
-	return (vec_add(cy->location, vec_multiply(cy->orientation, t)));
-}
+	/*t = (vec_dotp(ray.direction, cy->orientation) * vec_dotp(ray.direction, ray_to_cy)*/
+			/*- vec_dotp(cy->orientation, ray_to_cy) * vec_dotp(ray.direction, ray.direction)) / */
+		/*(vec_dotp(ray.direction, ray.direction) * vec_dotp(cy->orientation, cy->orientation)*/
+			 /*- pow(vec_dotp(ray.direction, cy->orientation), 2));*/
+	/*return (vec_add(cy->location, vec_multiply(cy->orientation, t)));*/
+/*}*/
 
 t_rayres		obj_dist_cylinder(t_object *cy, t_ray ray, t_info *info)
 {
-	t_vec3f ray_to_cy;
-	t_vec3f	p_ray;
-	t_vec3f	p_cy;
-	double	dist;
-	double	t_ray;
+	/*t_vec3f ray_to_cy;*/
+	/*t_vec3f	p_ray;*/
+	/*t_vec3f	p_cy;*/
+	/*double	dist;*/
+	/*double	t_ray;*/
 
 	(void)info;
 	if (float_compare(pow(vec_dotp(ray.direction, cy->orientation), 2),
 				vec_dotp(ray.direction, ray.direction) * vec_dotp(cy->orientation, cy->orientation)))
 		return (rayres_inf());
-	ray_to_cy = vec_from_to(ray.origin, cy->location);
-	t_ray = get_closest_ray(cy, ray, ray_to_cy);
-	if (t_ray < 0)
+	double dist = fabs(vec_dotp(vec_from_to(ray.origin, cy->location), vec_crossp(ray.direction, cy->orientation)))
+		/ vec_len(vec_crossp(ray.direction, cy->orientation));
+	/*ray_to_cy = vec_from_to(ray.origin, cy->location);*/
+	/*t_ray = get_closest_ray(cy, ray, ray_to_cy);*/
+	/*if (t_ray < 0)*/
+		/*return (rayres_inf());*/
+	/*p_ray = vec_add(ray.origin, vec_multiply(ray.direction, t_ray));*/
+	/*p_cy = get_closest_cy(cy, ray, ray_to_cy);*/
+	/*dist = vec_dist(p_ray, p_cy);*/
+	/*[>if (dist > cy->size / 2 || vec_dist(p_cy, cy->location) > cy->height / 2)<]*/
+	/*if (dist > cy->size / 2)*/
+		/*return (rayres_inf());*/
+	/*return (rayres_new_dist(cy, p_cy, cy->color, 0));*/
+	if (dist > cy->size / 2)
 		return (rayres_inf());
-	p_ray = vec_add(ray.origin, vec_multiply(ray.direction, t_ray));
-	p_cy = get_closest_cy(cy, ray, ray_to_cy);
-	dist = vec_dist(p_ray, p_cy);
-	if (dist > cy->size / 2 || vec_dist(p_cy, cy->location) > cy->height / 2)
-		return (rayres_inf());
-	return (rayres_new_dist(cy, p_cy, cy->color, 0));
+	return (rayres_new_dist(cy, vec_new(1,0,0), cy->color, 0));
 }
 
