caad6e0b35ecbfb3e50a6c92f0751ab820318507
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-03-03 17:04:51 +0100

    Wrong commit

--- src/vec/vec_new.c
+++ src/vec/vec_new.c
@@ -6,7 +6,7 @@
 /*   By: aaugusti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/27 15:52:00 by aaugusti          #+#    #+#             */
-/*   Updated: 2020/03/03 16:44:40 by aaugusti         ###   ########.fr       */
+/*   Updated: 2020/03/03 16:56:52 by aaugusti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,5 @@ t_vec3f			vec_new(double x, double y, double z)
 	res.x = x;
 	res.y = y;
 	res.z = z;
-	res.normalize = &vec_normalize;
 	return (res);
 }
