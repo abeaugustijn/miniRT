ced7e63bbd76e609eb29fa9fc609e4c4c48fe011
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-03-20 13:26:14 +0100

    Refactor: quat_rot

--- src/quat/quat_rot.c
+++ src/quat/quat_rot.c
@@ -6,7 +6,7 @@
 /*   By: aaugusti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/06 15:28:20 by aaugusti          #+#    #+#             */
-/*   Updated: 2020/03/06 16:16:57 by aaugusti         ###   ########.fr       */
+/*   Updated: 2020/03/20 13:13:33 by aaugusti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 t_vec4f	quat_rot(t_vec3f orientation, t_vec3f rot_dir, double angle)
 {
-	t_vec4f	p[2];
-	t_vec4f	r[2];
+	t_vec4f	p;
+	t_vec4f	q;
+	t_vec4f	q_inv;
 
-	p[0] = quat_new(1, orientation.x, orientation.y, orientation.z);
-	r[0] = quat_rot_local(rot_dir, angle);
-	r[1] = quat_inverse(r[0]);
-	return (quat_multiply(quat_multiply(r[0], p[0]), r[1]));
+	p = quat_new(0, orientation.x, orientation.y, orientation.z);
+	q = quat_rot_local(rot_dir, angle);
+	q_inv = quat_inverse(q);
+	return (quat_multiply(quat_multiply(q, p), q_inv));
 }
