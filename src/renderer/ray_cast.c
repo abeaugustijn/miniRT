89e61ae48eb0807778a7c6888628be6891533437
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-04-08 19:33:37 +0200

    Fix: norminette for ray_cast
    Resolves #27

--- src/renderer/ray_cast.c
+++ src/renderer/ray_cast.c
@@ -6,54 +6,47 @@
 /*   By: aaugusti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 16:30:00 by aaugusti          #+#    #+#             */
-/*   Updated: 2020/04/07 16:13:37 by aaugusti         ###   ########.fr       */
+/*   Updated: 2020/04/08 19:31:04 by aaugusti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ray_cast.h"
 #include <minirt.h>
 #include <math.h>
 #include <libft.h>
 
-static t_rayres	ray_cast_object(t_info *info, t_ray ray)
+static void		ray_cast_init_data(struct s_ray_cast_data *data)
 {
-	t_object	*current;
-	t_object	*closest;
-	double		min_distance;
-	double		dist;
-	size_t		i;
-	t_vec3f		normal;
-	t_vec3f		normal_closest;
-
-	min_distance = INFINITY;
-	normal = vec_new(0, 0, 0);
-	normal_closest = vec_new(0, 0, 0);
-	closest = NULL;
-	i = 0;
-	while (!vla_get_addr(info->objects, i, (void **)&current))
-	{
-		i++;
-		if (current->type == SQ)
-			continue;
-		dist = intersect(current, ray, &normal, info);
-		if (dist >= min_distance)
-			continue;
-		normal_closest = vec_is_normal(normal) ? normal : vec_new(0, 0, 0);
-		normal = vec_new(0, 0, 0);
-		closest = current;
-		min_distance = dist;
-	}
-	if (!closest)
-		return (rayres_inf());
-	return (rayres_new(closest, ray_point(ray, min_distance),
-				min_distance, normal_closest));
+	data->min_distance = INFINITY;
+	data->normal = vec_new(0, 0, 0);
+	data->normal_closest = vec_new(0, 0, 0);
+	data->closest = NULL;
 }
 
 t_color			ray_cast(t_info *info, t_ray ray)
 {
-	t_rayres	rayres;
+	size_t					i;
+	struct s_ray_cast_data	data;
 
-	rayres = ray_cast_object(info, ray);
-	if (rayres.dist == INFINITY)
+	ray_cast_init_data(&data);
+	i = 0;
+	while (!vla_get_addr(info->objects, i, (void **)&data.current))
+	{
+		i++;
+		if (data.current->type == SQ)
+			continue;
+		data.dist = intersect(data.current, ray, &data.normal, info);
+		if (data.dist >= data.min_distance)
+			continue;
+		data.normal_closest = vec_is_normal(data.normal) ?
+			data.normal : vec_new(0, 0, 0);
+		data.normal = vec_new(0, 0, 0);
+		data.closest = data.current;
+		data.min_distance = data.dist;
+	}
+	if (!data.closest)
 		return (col_new(0, 0, 0));
-	return (ray_cast_all_lights(info, rayres, ray));
+	return (ray_cast_all_lights(info, rayres_new(data.closest,
+					ray_point(ray, data.min_distance), data.min_distance,
+					data.normal_closest), ray));
 }
---
+++ src/renderer/ray_cast.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ray_cast.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaugusti <aaugusti@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/08 19:25:38 by aaugusti          #+#    #+#             */
+/*   Updated: 2020/04/08 19:26:34 by aaugusti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RAY_CAST_H
+# define RAY_CAST_H
+
+# include <minirt.h>
+
+struct	s_ray_cast_data {
+	double		dist;
+	double		min_distance;
+	t_object	*closest;
+	t_object	*current;
+	t_vec3f		normal;
+	t_vec3f		normal_closest;
+};
+
+#endif
